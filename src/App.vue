c3c9d56d91c9b704b67bac31339c96430e52cf67
raredead <smahelzed@gmail.com>
2023-10-23 15:37:10 +0700

    Подключение компонентов к основному проекту

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
 import AppHeader from './components/layout/AppHeader.vue';
+import BreadCrumbs from './components/layout/BreadCrumbs.vue'
 </script>
 
 <template>
     <div class="wrapper">
         <app-header />
+        <bread-crumbs 
+        :current="{href:'#', name: 'Проекты'}"
+        />
         <main>
             <router-view />
         </main>
