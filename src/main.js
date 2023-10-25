447f2604e5008f8600fd391c97f576fe1212121a
raredead <smahelzed@gmail.com>
2023-10-25 15:40:29 +0700

    Подключение компонентов в основной проект и подключение хранилища

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import './assets/style/global.scss';
+import './assets/style/global.scss'
 
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import router from './router';
+import router from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(createPinia())
+    .mount('#app')
--- src/views/AppProjects.vue
+++ src/views/AppProjects.vue
@@ -1,3 +1,45 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useProjectsStore } from '../stores/ProjectsStore';
+
+import FilterButton from '../components/UI/FilterButton.vue';
+import ProjectItem from '../components/ProjectItem.vue';
+
+const projectsStore = useProjectsStore()
+
+const title = ref('Проекты')
+
+onMounted(() => {
+    projectsStore.projectList()
+})
+</script>
+
 <template>
-    <div></div>
+    <section class="projects">
+        <h1 class="projects__title">{{ title }}</h1>
+        <div class="projects__btns">
+            <filter-button 
+            name="Backend"
+            />
+            <filter-button 
+            name="Frontend"
+            />
+        </div>
+        <div class="projects__elements">
+            <project-item 
+            v-for="project in projectsStore.projects"
+            :key="project.id"
+            :project="project"/>
+        </div>
+    </section>
 </template>
+
+<style lang="scss" scoped>
+.projects {
+    &__elements {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 40px;
+    }
+}
+</style>
