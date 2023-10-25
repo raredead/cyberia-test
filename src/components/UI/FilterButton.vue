6398fc2e956365f0dd1b41af4b66e6236fbdff7e
raredead <smahelzed@gmail.com>
2023-10-25 15:38:44 +0700

    Папка юзабилити компонентов

---
+++ src/components/UI/FilterButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+    name: {
+        type: String,
+        default: 'Btn'
+    },
+})
+</script>
+
+<template>
+    <button class="filter-btn">
+        {{ props.name }}
+    </button>
+</template>
