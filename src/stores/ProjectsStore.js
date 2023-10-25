801308d2048cca8751b5fc153d454b9794a74538
raredead <smahelzed@gmail.com>
2023-10-25 17:47:23 +0700

    Переписал stote на composition api

--- src/stores/ProjectsStore.js
+++ src/stores/ProjectsStore.js
@@ -1,16 +1,20 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 const URL = 'https://backend.cyberia.studio/api/v1/projects'
 
-export const useProjectsStore = defineStore('projectsStore', {
-    state: () => ({
-        projects: []
-    }),
+export const useProjectsStore = defineStore('projectsStore', () => {
+    const projects = ref([])
+    const loader = ref(false)
 
-    actions: {
-        async projectList () {
-            const res = await fetch(URL)
-            const data = await res.json()
-            this.projects = data.items
-        }
+    const projectList = async() => {
+        loader.value = true
+        const res = await fetch(URL)
+        const data = await res.json()
+        projects.value = data.items
+        loader.value = false
+    }
+
+    return {
+        loader, projects, projectList
     }
 })
